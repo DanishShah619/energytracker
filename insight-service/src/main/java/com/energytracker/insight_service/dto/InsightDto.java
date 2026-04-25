1178678c71906cb770ea81bf10a147882cd11037
Danish Shah <shanildanshah@gmail.com>
2026-04-25 23:01:04 +0530

    insight done

---
+++ insight-service/src/main/java/com/energytracker/insight_service/dto/InsightDto.java
@@ -0,0 +1,12 @@
+package com.energytracker.insight_service.dto;
+
+
+import lombok.Builder;
+
+@Builder
+public record InsightDto(
+        Long userId,
+        String tips,
+        double energyUsage
+) {
+}
